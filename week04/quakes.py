ba016c3767d206a088d660e5f74d886c4094149c
JingJiao <ucapjji@ucl.ac.uk>
2020-10-29 18:14:51 +0800

    added some documents

--- week04/quakes.py
+++ week04/quakes.py
@@ -2,18 +2,43 @@
 
 # At the top of the file, import any libraries you will use.
 # import ...
+import requests
+import json
 
+quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                      params={
+                          'starttime': "2000-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2018-10-11",
+                          "orderby": "time-asc"}
+                      )
 # If you want, you can define some functions to help organise your code.
 # def helper_function(argument_1, argument_2):
 #   ...
 
+earthquakes_data = json.loads(quakes.text)
+
 # When you run the file, it should print out the location and magnitude
 # of the biggest earthquake.
 # You can run the file with `python quakes.py` from this directory.
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # ...do things here to find the results...
+with open('earthquakes_data.json','w') as target:
+    target.write(quakes.text)
 
+max_magnitude = 0
+index = 0
+for i in range(len(earthquakes_data['features'])):
+    test = earthquakes_data['features'][i]['properties']['mag']
+    if test > max_magnitude:
+        max_magnitude = test
+        index = i
+coords = earthquakes_data['features'][index]['geometry']['coordinates']
     # The lines below assume that the results are stored in variables
     # named max_magnitude and coords, but you can change that.
-    print(f"The maximum magnitude is {max_magnitude} "
-          f"and it occured at coordinates {coords}.")
+print(f"The maximum magnitude is {max_magnitude} ")
+print(f"and it occured at coordinates {coords}.")
